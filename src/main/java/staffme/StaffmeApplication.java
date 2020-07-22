9b6271e6ef41453b71467924af70999c73a54a61
AlexGeorgiev86 <hankyz@gmail.com>
2020-07-22 10:54:36 +0300

    Initial commit

---
+++ src/main/java/staffme/StaffmeApplication.java
@@ -0,0 +1,13 @@
+package staffme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/staffme/StaffmeApplicationTests.java
@@ -0,0 +1,13 @@
+package staffme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
